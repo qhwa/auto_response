0e154e0cdf59bf620ba5b2f97dde45b0f82abe45
qhwa <qhwa@163.com>
2012-12-24 12:53:28 +0800

    save

--- lib/ar/rule_dsl.rb
+++ lib/ar/rule_dsl.rb
@@ -1,15 +1,15 @@
 module AutoResp
   module Helpers
     def url(target, &block)
-      ::AutoResp.add_rule(target, &block);
+      ::AutoResp::RuleManager.add_rule(target, &block);
     end
 
     def r(resp)
-      ::AutoResp.add_handler( resp )
+      ::AutoResp::RuleManager.add_handler( resp )
     end
 
     def goto(url)
-      ::AutoResp.add_handler '=GOTO=> ' << url
+      ::AutoResp::RuleManager.add_handler '=GOTO=> ' << url
     end
   end
 end
